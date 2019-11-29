2f2de03d5d2cedf8cfdcf38029a21a65c257d919
Georgly <korsakov.georgy.s@gmail.com>
2019-11-29 12:01:46 +0300

    First lab

--- projects/lab1/main.c
+++ projects/lab1/main.c
@@ -1,12 +1,17 @@
 #include <stm32f4xx.h>
 
 /*
- * @brief Simple lab -- blink leds in R->B->G order, on button press
- *        LEDS blink white once for a short period of time and then
- *        blink in a reverse order G->B->R.
+ * @brief 
+ *        
+ *        
  */
 
-#define SWITCH_DELAY    ((uint32_t)200000)
+#define SWITCH_DELAY    ((uint32_t)400000)
+#define PLL_N     336
+#define PLL_P     4
+#define PLL_Q     7
+#define PLL_M_HSE 10
+#define PLL_M_HSI 20
 
 typedef enum blink_mul_e
 {
@@ -16,79 +21,192 @@ typedef enum blink_mul_e
 
 void blink_led(GPIO_TypeDef * port, uint16_t pins, blink_mul_t multiplier);
 
-void set_clock(uint32_t pins)
-{
-    RCC->CR |= pins;
-}
+void turn_on_led(GPIO_TypeDef * port, uint16_t pins, blink_mul_t multiplier);
 
-void config_hse_pll()
-{
-  RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE;
-  RCC->PLLCFGR |= 
-}
+void clock_init(void);
 
-void switch_clock()
-{
-    if ((RCC->CFGR & RCC_CFGR_SWS) == RCC_CFGR_SWS_HSE)
-    {
-        RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
-        RCC->CFGR |= RCC_CFGR_SW_HSI;
-        return;
-    }
-    RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
-    RCC->CFGR |= RCC_CFGR_SW_HSE;
-}
+/*
+* int8_t clock - type of source clock for PLL. 1 - HSE, 0 - HSI
+*/
+void set_clock(int8_t clock);
+
+void switch_clock(void);
 
 int main(void)
 {
-  while( (RCC->CR & RCC_CR_HSERDY) != RCC_CR_HSERDY);
-  set_clock(RCC_CR_HSEON);
+  clock_init();
+  set_clock(1);
   GPIO_InitTypeDef GPIO_InitStructure;
 
   /* Enable peripheral clock for LEDs and buttons port */
   RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
   /* Init LEDs */
-  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10;
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_OUT;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_NOPULL;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
 
-  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0 | GPIO_Pin_1;
+  GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_0;
   GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
-  GPIO_Init(GPIOE, &GPIO_InitStructure);
-
+  GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_DOWN;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
   /* Turn all the leds off */
-  GPIO_SetBits(GPIOA, GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10);
-
-  int8_t i = 0;
-  int8_t direction = 1;
-
+  GPIO_SetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+  
+  uint8_t i = 0, pushed = 0;
+    
   while (1)
   {
-    int state = GPIO_ReadInputDataBit(GPIOE, GPIO_Pin_0);
-    if (!state)
+    int state = GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0);
+    if (pushed == 0)
     {
+      if (state == 1)
+      {
+        pushed = 1;
         switch_clock();
-      //direction = -direction;
+      }
+    }
+    else
+    {
+      if (!state)
+      {
+        pushed = 0;
+      }
+    }
+    blink_led(GPIOD, GPIO_Pin_12 << i, BLINK_MUL_LONG);
+    turn_on_led(GPIOD, GPIO_Pin_12 << i, BLINK_MUL_LONG);
+    i = (i + 1) % 4;
+  }
+}
 
-      //blink_led(GPIOA, GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10, BLINK_MUL_FAST);
+void clock_init(void)
+{
+  /* Reset RCC_CR register */
+  RCC->CR = 0x00000081;
+  /* Reset RCC_CFGR register*/
+  RCC->CFGR = 0x00000000;
+  RCC->CR &= (uint32_t)0xFEF6FFFF;
+  /* Reset RCC_PLLCFGR register*/
+  RCC->PLLCFGR = 0x24003010;
+  RCC->CR &= (uint32_t)0xFFFBFFFF;
+}
+
+void set_clock(int8_t clock)
+{
+  uint32_t StartUpCounter = 0, Status = 1, Pll_src = RCC_PLLCFGR_PLLSRC_HSE, Pll_m = PLL_M_HSE;
+  uint32_t tmp = 0;
+  if(clock)
+  {
+    RCC->CR |= ((uint32_t)RCC_CR_HSEON);
+    
+    /* Wait till HSE is ready and if Time out is reached exit */
+    do
+    { 
+      Status = RCC->CR & RCC_CR_HSERDY;
+      StartUpCounter++;
+    } while((Status == 0) && (StartUpCounter != HSE_STARTUP_TIMEOUT));
+  
+    if ((RCC->CR & RCC_CR_HSERDY) != RESET)
+    { 
+      Status = (uint32_t)0x01;
+    }
+    else
+    { 
+      Status = (uint32_t)0x00;
     }
 
-    blink_led(GPIOA, GPIO_Pin_8 << i, BLINK_MUL_LONG);
+  }
+  else
+  {
+    Pll_src = RCC_PLLCFGR_PLLSRC_HSI;
+    Pll_m = PLL_M_HSI;
+    tmp = 1;
 
-    //i = (3 + (i - direction) % 3) % 3;
-    i = (i + 1) % 3;
+    if ((RCC->CR & RCC_CR_HSIRDY) != RESET)
+    {
+      Status = (uint32_t)0x01;
+    }
+    else
+    {
+      Status = (uint32_t)0x00;
+    }
+  }
+
+  if (Status == (uint32_t)0x01)
+  {
+
+    /* Select regulator voltage output Scale 1 mode */
+    RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+    PWR->CR |= PWR_CR_VOS;
+ 
+    /* HCLK = SYSCLK / 1*/
+    RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
+
+    /* PCLK2 = HCLK / 2*/
+    RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
+ 
+    /* PCLK1 = HCLK / 4*/
+    RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+
+
+    /* Configure the main PLL */
+    RCC->PLLCFGR = Pll_m | (PLL_N << 6) | (((PLL_P >> 2) - tmp) << 16) |
+                   (Pll_src) | (PLL_Q << 24); 
+
+    /* Enable the main PLL */
+    RCC->CR |= RCC_CR_PLLON;
+    
+    /* Wait till the main PLL is ready */
+    while((RCC->CR & RCC_CR_PLLRDY) == 0)
+    {
+    }
+    
+    /* Select the main PLL as system clock source */
+    RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
+    RCC->CFGR |= RCC_CFGR_SW_PLL;
+
+    /* Wait till the main PLL is used as system clock source */
+    while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS ) != RCC_CFGR_SWS_PLL)
+    {
+    }
+
+  }
+  else
+  {
+    GPIO_SetBits(GPIOD, GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15);
+  }
+}
+
+void switch_clock()
+{
+  if ((RCC->PLLCFGR & RCC_PLLCFGR_PLLSRC) == RCC_PLLCFGR_PLLSRC_HSE)
+  {
+    clock_init();
+    set_clock(0);
+  }
+  else if ((RCC->PLLCFGR & RCC_PLLCFGR_PLLSRC) == RCC_PLLCFGR_PLLSRC_HSI)
+  {
+    clock_init();
+    set_clock(1);
   }
 }
 
 void blink_led(GPIO_TypeDef * port, uint16_t pins, blink_mul_t multiplier)
+{
+  uint32_t k = 0;
+
+  GPIO_ResetBits(port, pins);
+  for (k = 0; k < SWITCH_DELAY * multiplier; ++k);
+  GPIO_SetBits(port, pins);
+}
+
+void turn_on_led(GPIO_TypeDef * port, uint16_t pins, blink_mul_t multiplier)
 {
   uint32_t k = 0;
 
